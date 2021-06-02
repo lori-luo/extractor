b29f97611d7b7f7cf249f888653b522b039d599f
Richard Bernisca <taropogi_123@yahoo.com>
2021-06-02 22:47:35 +0800

    comment or remove upload component

--- resources/views/components/upload-file.blade.php
+++ resources/views/components/upload-file.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <form action="{{ route('xml_pub_med.upload.store') }}" id="form-upload" enctype="multipart/form-data" method="post">
+    <!--  <form action="{{ route('xml_pub_med.upload.store') }}" id="form-upload" enctype="multipart/form-data" method="post">
         @csrf
         <h1>Upload</h1>
         <div class="form-group">
@@ -9,4 +9,5 @@
 
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+    -->
 </div>

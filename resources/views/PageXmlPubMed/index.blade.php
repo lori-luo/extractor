43d872e5717a6211744b2c0bedb4a7fb7c6853ea
Richard Bernisca <taropogi_123@yahoo.com>
2021-06-03 21:14:12 +0800

    checkout to d95db24

--- resources/views/PageXmlPubMed/index.blade.php
+++ resources/views/PageXmlPubMed/index.blade.php
@@ -4,10 +4,11 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
                 <x-upload-file :category="'PubMed'" />
+                <h1>List of Files here</h1>
                 <table class="table">
                     <thead>
                         <tr>

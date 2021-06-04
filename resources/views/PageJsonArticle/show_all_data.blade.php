89e4ff7e551794389b58e596d54c63276d7b2e33
Richard Bernisca <taropogi_123@yahoo.com>
2021-06-04 23:30:03 +0800

    change logo

--- resources/views/PageJsonArticle/show_all_data.blade.php
+++ resources/views/PageJsonArticle/show_all_data.blade.php
@@ -7,6 +7,15 @@
         <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="table-responsive">
+                    <nav class="navbar navbar-light bg-light">
+                        <div class="container-fluid">
+                            <a class="navbar-brand">Navbar</a>
+                            <form class="d-flex">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </nav>
                     <table class="table table-sm text-nowrap table-hover">
                         <thead class="table-primary">
                             <tr>

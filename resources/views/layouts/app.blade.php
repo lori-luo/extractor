c226959ad72a0fabb7476d224562d296c9f2d80d
Richard Bernisca <taropogi_123@yahoo.com>
2021-06-17 13:15:38 +0800

    add meta desc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+
+    <meta name="description" content="@yield('title','Importer')">
+
     <title>@yield('title','Importer')</title>
 
     <!-- Fonts -->

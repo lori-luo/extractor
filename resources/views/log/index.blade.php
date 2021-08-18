fae5aa805769c0f6a82ec6c0c5ced0d81dcbd848
Richard Bernisca <taropogi_123@yahoo.com>
2021-08-18 22:19:43 +0800

    log padding

--- resources/views/log/index.blade.php
+++ resources/views/log/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="mt-2">
         <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5 pt-0">
                 <livewire:page-log />
             </div>
         </div>
